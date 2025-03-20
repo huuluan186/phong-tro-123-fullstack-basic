e6a33c3923308dd2d6c46b0c3fae3ac0b9d09749
huuluan186 <luanphamhuu2004@gmail.com>
2025-03-20 11:26:10 +0700

    initRoutes

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 require('dotenv').config()
 import cors from 'cors'
 const app = express()
-
+import initRoutes from './src/routes';
 app.use(cors({
     origin:process.env.CLIENT_URL,
     methods:["POST","GET","PUT","DELETE"]
@@ -11,7 +11,7 @@ app.use(cors({
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
-app.use('/',(req,res)=>{res.send("server on ...")})
+initRoutes(app)
 
 const port=process.env.PORT || 8888
 app.listen(port,()=>{
