7cb117cf76846af6f898b98207c61f51b061631f
huuluan186 <luanphamhuu2004@gmail.com>
2025-03-21 11:55:38 +0700

    import redux-thunk

--- client/src/redux.js
+++ client/src/redux.js
@@ -1,9 +1,11 @@
 import rootReducer from "./store/reducers/rootReducer";
 import { persistStore } from "redux-persist";
-import { legacy_createStore as createStore } from "redux";
+import { legacy_createStore as createStore, applyMiddleware} from "redux";
+import { thunk } from "redux-thunk";
 
 const reduxStore=()=>{
-    let store = createStore(rootReducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+    let store = createStore(rootReducer,applyMiddleware(thunk))
+    //,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     let persistor = persistStore(store)
     return {store,persistor}
 }
