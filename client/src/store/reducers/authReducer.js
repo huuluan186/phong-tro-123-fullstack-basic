6e16cb62b92e43d074bd9b85dcfe784bada7b801
huuluan186 <luanphamhuu2004@gmail.com>
2025-03-21 12:05:25 +0700

    them case cho register thanh cong va that bai

--- client/src/store/reducers/authReducer.js
+++ client/src/store/reducers/authReducer.js
@@ -1,10 +1,25 @@
+import actionTypes from "../actions/actionTypes";
+
 const initState = {
     isLoggedIn:false,
     token:null
 }
 
 const authReducer = (state=initState,actions)=>{
-    switch(actions.types){
+    switch(actions.type){
+        case actionTypes.REGISTER_SUCCESS:
+            return{
+                ...state,
+                isLoggedIn: true,
+                token: actions.data
+            }
+        case actionTypes.REGISTER_FAIL:
+            return{
+                ...state,
+                isLoggedIn: false,
+                msg: actions.data,
+                token:null
+            }
         default:
             return state;
     }
