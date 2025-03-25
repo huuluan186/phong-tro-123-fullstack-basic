7eab654b8e80a2f97c78397ba49ec941dcd3d628
huuluan186 <luanphamhuu2004@gmail.com>
2025-03-25 21:20:56 +0700

    xu li validate data va hoan thanh chuc nang dang nhap/dang ki/dang xuat

--- client/src/containers/Public/Header.js
+++ client/src/containers/Public/Header.js
@@ -5,9 +5,13 @@ import {Button} from "../../components";
 import icons from "../../utils/icon";
 import {Link, useNavigate} from 'react-router-dom';
 import { path } from "../../utils/constant";
+import { useSelector, useDispatch } from "react-redux";
+import * as actions from '../../store/actions'
 const {CiCirclePlus}=icons
 
 const Header = () => {
+    const {isLoggedIn}=useSelector(state=>state.auth)
+    const dispatch = useDispatch()
     const navigate = useNavigate()
     const goLogin = useCallback((flag)=>{
         navigate(path.LOGIN,
@@ -21,9 +25,20 @@ const Header = () => {
         <div className="w-full flex items-center justify-between">
             <Link to={'/'}><img src={logo} alt="logo" className='w-[240px] h-[70px] object-contain'/></Link>
             <div className='flex items-center gap-2'>
-                <small>Phongtro123.com xin chào!</small>
-                <Button text={'Đăng nhập'} textColor='text-white' bgColor='bg-[#3961fb]' onClick={()=>goLogin(false)}/>
-                <Button text={'Đăng ký'} textColor='text-white' bgColor='bg-[#3961fb]' onClick={()=>goLogin(true)}/>
+                {!isLoggedIn &&
+                    <div className="flex items-center gap-2">
+                    <small>Phongtro123.com xin chào!</small>
+                    <Button text={'Đăng nhập'} textColor='text-white' bgColor='bg-[#3961fb]' onClick={()=>goLogin(false)}/>
+                    <Button text={'Đăng ký'} textColor='text-white' bgColor='bg-[#3961fb]' onClick={()=>goLogin(true)}/>
+                </div>
+                }
+
+                {isLoggedIn &&
+                    <div className="flex items-center gap-2">
+                    <small>Phongtro123.com xin chào Ten!</small>
+                    <Button text={'Đăng xuất'} textColor='text-white' bgColor='bg-red-700' onClick={()=> dispatch(actions.logout())}/>
+                </div>
+                }
                 <Button text={'Đăng tin mới'} textColor='text-white' bgColor='bg-secondary2' IcAfter={CiCirclePlus}/>     
             </div>
         </div>
--- client/src/containers/Public/Login.js
+++ client/src/containers/Public/Login.js
@@ -1,16 +1,19 @@
 import React,{useState,useEffect} from "react";
 import {Button,InputForm} from '../../components'
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { apiRegister } from "../../services/auth";
 import * as actions from '../../store/actions'
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector} from "react-redux";
 
 const Login = () => {
 
     const location = useLocation()
     const dispatch = useDispatch() 
-
+    const navigate = useNavigate()
+    const {isLoggedIn} =useSelector(state=>state.auth) // .auth do define trong rootReducer 
     const [isRegister,setIsRegister]= useState(location.state?.flag) //?: state có giá trị mới trỏ đến flag
+
+    const [invalidFields, setInvalidFields] = useState([])
 
     useEffect(() => {
         // Cập nhật giá trị của isRegister bằng flag từ location.state (nếu có)
@@ -19,6 +22,10 @@ const Login = () => {
         }
     }, [location.state?.flag]); // Chạy lại khi location.state?.flag thay đổi
     
+    useEffect(()=>{
+        isLoggedIn && navigate('/')
+    },[isLoggedIn])
+
     const [payload,setPayLoad] = useState({
         phone:'',
         password:'',
@@ -26,26 +33,89 @@ const Login = () => {
     })
 
     const handleSubmit = async () => {
-        console.log(payload);
-        await isRegister ? dispatch(actions.register(payload)) : dispatch(actions.login(payload));
+        let finalPayload = isRegister ? payload : {
+            phone: payload.phone,
+            password: payload.password
+        }
+        let invalids=validate(finalPayload)
+        if(invalids===0) await isRegister ? dispatch(actions.register(payload)) : dispatch(actions.login(payload));
+        console.log("invalids: ",invalids)
+        
     };
-    
+
+    const validate = (payload) => {
+        let invalids=0
+        let fields = Object.entries(payload)
+        fields.forEach(item =>{
+            if(item[1]===''){
+                setInvalidFields(prev => [...prev, {
+                    name: item[0],
+                    message: 'Bạn không được bỏ trống trường này.'
+                }])
+                invalids++
+            } 
+        })
+
+        fields.forEach(item => {
+            switch(item[0]){
+                case 'password':
+                    if(item[1].length < 6){
+                        setInvalidFields(prev => [...prev, {
+                            name: item[0],
+                            message: 'Mật khẩu phải có tối thiểu 6 kí tự.'
+                        }])
+                        invalids++
+                    }
+                    break;
+                case 'phone':
+                    if(!+item[1]){
+                        setInvalidFields(prev => [...prev, {
+                            name: item[0],
+                            message: 'Số điện thoại không hợp lệ.'
+                        }])
+                        invalids++
+                    }
+                    break;
+
+                default:
+                    break
+            }
+        })
+
+        //console.log(payload)
+        return invalids;
+    }
 
     return(
         <div className="w-full bg-white max-w-600 p-[30px] pb-[100px] rounded-md shadow-sm ">
             <h3 className="font-semibold text-2xl mb-3 text-center">{isRegister ? 'Đăng ký tài khoản' : 'Đăng nhập'}</h3>
             <div className="w-full flex flex-col gap-5">
-                {isRegister &&  <InputForm label={'Họ Tên'} value={payload.name} setValue={setPayLoad} type={'name'}/> }
-                <InputForm label={'Số điện thoại'} value={payload.phone} setValue={setPayLoad} type={'phone'}/>
-                <InputForm label={'Mật khẩu'} value={payload.password} setValue={setPayLoad} type={'password'}/>
+                {isRegister &&  <InputForm invalidFields={invalidFields} setInvalidFields={setInvalidFields} label={'Họ Tên'} value={payload.name} setValue={setPayLoad} type={'name'}/> }
+                <InputForm invalidFields={invalidFields} setInvalidFields={setInvalidFields} label={'Số điện thoại'} value={payload.phone} setValue={setPayLoad} type={'phone'}/>
+                <InputForm invalidFields={invalidFields} setInvalidFields={setInvalidFields} label={'Mật khẩu'} value={payload.password} setValue={setPayLoad} type={'password'}/>
                 <Button text={isRegister ? 'Đăng ký' : 'Đăng nhập'} bgColor='bg-secondary2' text-color="text-white" fullWidth onClick={handleSubmit}/>
             </div>
             <div className="mt-7 flex items-center justify-between">
-               {isRegister ? <small>Bạn đã có tài khoản? <span onClick={()=>setIsRegister(false)} className="text-blue-500 hover:underline cursor-pointer">Đăng nhập ngay</span></small>
+               {isRegister ? <small>Bạn đã có tài khoản? 
+                <span onClick={()=>{
+                setIsRegister(false) 
+                setPayLoad({
+                 phone:'',
+                 password:'',
+                 name:''
+               })}} className="text-blue-500 hover:underline cursor-pointer">Đăng nhập ngay</span>
+               </small>
                :
                 <>
                      <small className="text-blue hover:text-[red] cursor-pointer" >Bạn quên mật khẩu?</small>
-                     <small onClick={()=>setIsRegister(true)} className="text-blue hover:text-[red] cursor-pointer">Tạo tài khoản mới</small>
+                     <small onClick={()=>{
+                        setIsRegister(true) 
+                        setPayLoad({
+                        phone:'',
+                        password:'',
+                        name:''
+                        })}} className="text-blue hover:text-[red] cursor-pointer">Tạo tài khoản mới
+                    </small>
                 </>
                }
             </div>
